b7d4ed43949c915ad18132b20db505811d08328e
Yannick <yannick8610@icloud.com>
2024-01-03 18:44:47 +0100

    bird fly

--- flappy-bird/bird.js
+++ flappy-bird/bird.js
@@ -1,11 +1,19 @@
 function Bird() {
-    const FLY = 5;
+    const FLY = 40;
 
     this.x = w * 0.4;
     this.y = h / 2;
     this.r = 10;
+    this.velocity = 0.1;
 
     this.draw = function () {
+        this.y = this.y + this.velocity;
+        if (this.y >= h - this.r) {
+            this.y = h - this.r;
+        } else {
+            this.velocity = this.velocity + 0.1;
+        }
+
         fill(255, 0, 0);
         ellipseMode(CENTER);
         strokeWeight(0)
@@ -15,6 +23,7 @@ function Bird() {
 
     this.fly = function () {
         this.y = this.y - FLY;
+        this.velocity = -1;
     }
 
 }
